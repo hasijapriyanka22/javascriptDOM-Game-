350d9ed6ac394fff59dd76b71218d4f6c49ef169
priyankahasija <priyanka.hasija@msystechnologies.com>
2019-08-30 16:59:15 +0530

    updated

--- script.js
+++ script.js
@@ -11,14 +11,58 @@ GAME RULES:
 
 
 var scores, roundScore, activePlayer, gamePlaying;
-scores = [0,0];
-roundScore = 0 ;
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
 
-newGame(){
-    console.log('new game');
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+function onNewGame() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
 }
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
+
+function onRolling() {
+
+    //Getting a random number
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    //Updating the image 
+    document.querySelector('#current-' + activePlayer).textContent = dice;
+    diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //Updating the round score
+    roundScore = scores[activePlayer];
+    scoreDOM = document.getElementById('score-' + activePlayer);
+    roundScore += dice;
+    scoreDOM.textContent = roundScore;
+    
+    if (dice == 1) {
+        activePlayer == 1 ? scoractivePlayer = 0 : activePlayer = 1;
+    }
+
+}
+
+function onHold() {
+    activePlayer == 1 ? activePlayer = 0 : activePlayer = 1;
+}
+
+
+
+
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 //var x = document.querySelector('#score-0').textContent;
 
